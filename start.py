0e96026773ee6ee1314a900dcb9ee0a197e3ffbe
futuhul26 <55523754+futuhul26@users.noreply.github.com>
2020-04-20 08:38:02 +0700

    Update start.py

--- start.py
+++ start.py
@@ -1,14 +1,4 @@
-from mnist import MNIST
-import numpy as np
-from lib import *
-
-mndata = MNIST('.data')
+from mnist.loader import MNIST
+mndata = MNIST('./data')
 images, labels = mndata.load_training()
 t_images, t_labels = mndata.load_testing()
-
-images = np.array(images)
-t_images = np.array(t_images)
-labels = np.array(labels)
-t_labels = np.array(t_labels)
-
-print_statistics(images, t_images, labels, t_labels)
